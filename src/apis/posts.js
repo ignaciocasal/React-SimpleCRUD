62cb6b8cf141d025dfad2fe3b569e89d2438d6ee
Ignacio <ignaciocasaldev@gmail.com>
2021-03-25 11:31:27 -0300

    Changed api url

--- src/apis/posts.js
+++ src/apis/posts.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export default axios.create({
-    // baseURL: 'https://my-json-server.typicode.com/ignaciocasal/my-json-api'
-    baseURL: 'http://localhost:3001'
+    baseURL: 'https://my-json-server.typicode.com/ignaciocasal/my-json-api'
+    // baseURL: 'http://localhost:3001'
 })
